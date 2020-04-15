54afbc9c5d121fcb7ffdc48dbe60c1226525705b
rubinhosilva <08101201rJ>
2020-04-15 00:12:18 -0300

    cadastro de device

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -34,7 +34,7 @@ class DeviceController extends Controller
 
         $coordenadas = explode(',', $data['localizacao']);
         $resultado = app('geocoder')->reverse($coordenadas[0], $coordenadas[1])->get();
-        var_dump($resultado);
+        var_dump($resultado[0]);
         //latitude, longitude
 
         $localizacao = new Localizacao();
