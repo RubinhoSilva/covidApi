7e270aa6baaeaa8bcc6b3ec358a068e60aa0feaa
rubinhosilva <08101201rJ>
2020-04-24 19:55:33 -0300

    teste de envio de notificacao

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::post('/cadastrarDevice', 'DeviceController@cadastrar');
-Route::post('/teste', 'DeviceController@teste');
+Route::get('/teste', 'DeviceController@teste');
 
 Route::middleware(['guard.jwt:device', 'jwt.auth'])->group(function () {
     Route::post('/atualizarLocalizacao', 'LocalizacaoController@atualizar');
