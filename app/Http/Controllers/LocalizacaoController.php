8104e22857582eb8b49bcc807fe7311ce4086248
rubinhosilva <08101201rJ>
2020-06-28 23:07:06 -0300

    add if para vericar se existe dado no bd

--- app/Http/Controllers/LocalizacaoController.php
+++ app/Http/Controllers/LocalizacaoController.php
@@ -38,31 +38,35 @@ class LocalizacaoController extends Controller
         $device = Device::find(Auth::id());
 
         $localCidade = $device->localizacoes()->orderBy('idLocalizacao', 'desc')->first();
-        $coordenadasAntigas = explode(',', $localCidade->dados);
-        $coordenadasNovas = explode(',', $data['localizacao']);
-        $km = Haversini::calculate(
-            $coordenadasAntigas[0],
-            $coordenadasAntigas[1],
-            $coordenadasNovas[0],
-            $coordenadasNovas[1],
-            'km'
-        );
 
-        if ($km <= 4) {
-            $cidade = $localCidade->cidade;
-        } else {
-            $localCidade = Localizacao::where('dados', $data['localizacao'])->orderBy('idLocalizacao', 'desc')->first();
+        if($localCidade != null){
+            $coordenadasAntigas = explode(',', $localCidade->dados);
+            $coordenadasNovas = explode(',', $data['localizacao']);
+            $km = Haversini::calculate(
+                $coordenadasAntigas[0],
+                $coordenadasAntigas[1],
+                $coordenadasNovas[0],
+                $coordenadasNovas[1],
+                'km'
+            );
 
-            if ($localCidade != null) {
+            if ($km <= 4) {
                 $cidade = $localCidade->cidade;
             } else {
-                $coordenadas = explode(',', $data['localizacao']);
-                $resultado = app('geocoder')->reverse($coordenadas[0], $coordenadas[1])->toJson();
-                //latitude, longitude
+                $localCidade = Localizacao::where('dados', $data['localizacao'])->orderBy('idLocalizacao', 'desc')->first();
 
-                $cidade = json_decode($resultado)->properties->locality;
+                if ($localCidade != null) {
+                    $cidade = $localCidade->cidade;
+                } else {
+                    $coordenadas = explode(',', $data['localizacao']);
+                    $resultado = app('geocoder')->reverse($coordenadas[0], $coordenadas[1])->toJson();
+                    //latitude, longitude
+
+                    $cidade = json_decode($resultado)->properties->locality;
+                }
             }
         }
+
 
 
         $localizacao = new Localizacao();
@@ -73,7 +77,7 @@ class LocalizacaoController extends Controller
         $localizacao->save();
 
         return response()->json([
-            'mensagem' => "Atualizado com sucesso!",
+            'mensagem' => "Atualizado com sucesso!"
         ], 200, $header);
     }
 
